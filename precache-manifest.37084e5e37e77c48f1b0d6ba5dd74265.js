e9e619bcc61d794ad8beeabc57abc712e2c93d85
AdarshJaso <adarsh.jasoliya@gmail.com>
2020-07-29 16:03:34 +0530

    Updates

---
+++ precache-manifest.37084e5e37e77c48f1b0d6ba5dd74265.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7140d8e0d58ff9a8b0210bd8519d567",
+    "url": "/ReactJs-Website/index.html"
+  },
+  {
+    "revision": "02457148548845413c4b",
+    "url": "/ReactJs-Website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "34fede20d3e0371e9379",
+    "url": "/ReactJs-Website/static/css/main.1b9e5adc.chunk.css"
+  },
+  {
+    "revision": "02457148548845413c4b",
+    "url": "/ReactJs-Website/static/js/2.71e34690.chunk.js"
+  },
+  {
+    "revision": "7262f9d2c1a7c941a84f9018e39ff673",
+    "url": "/ReactJs-Website/static/js/2.71e34690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fede20d3e0371e9379",
+    "url": "/ReactJs-Website/static/js/main.73133f62.chunk.js"
+  },
+  {
+    "revision": "f6e42705da63cb637024",
+    "url": "/ReactJs-Website/static/js/runtime-main.63af89bd.js"
+  },
+  {
+    "revision": "119379cf217854b69ea567f46b0062eb",
+    "url": "/ReactJs-Website/static/media/1.119379cf.jpg"
+  },
+  {
+    "revision": "f7c9874b19b911ecfd2854bc9943dc6d",
+    "url": "/ReactJs-Website/static/media/2.f7c9874b.png"
+  },
+  {
+    "revision": "c7e1bcb4f92bd6464b2622d1781979f3",
+    "url": "/ReactJs-Website/static/media/22.c7e1bcb4.jpg"
+  },
+  {
+    "revision": "7d231a38e82c27a920e9c49d03cd463e",
+    "url": "/ReactJs-Website/static/media/3.7d231a38.png"
+  },
+  {
+    "revision": "a24c164350af4c70f1e31ec09b9400d8",
+    "url": "/ReactJs-Website/static/media/4.a24c1643.png"
+  },
+  {
+    "revision": "095b879c89bcca0720f5ee22f8109f07",
+    "url": "/ReactJs-Website/static/media/44.095b879c.jpg"
+  },
+  {
+    "revision": "4ded96fd001b82ccb8ed852fcc360dfc",
+    "url": "/ReactJs-Website/static/media/5.4ded96fd.png"
+  },
+  {
+    "revision": "baf81937d0dba0b4201365c7df3cdbb0",
+    "url": "/ReactJs-Website/static/media/6.baf81937.png"
+  },
+  {
+    "revision": "006774d3de910b208cda7208fdb1a09f",
+    "url": "/ReactJs-Website/static/media/7.006774d3.png"
+  },
+  {
+    "revision": "dcab7b1f2c643cee15db3769270111c5",
+    "url": "/ReactJs-Website/static/media/8.dcab7b1f.jpg"
+  },
+  {
+    "revision": "50c26525be3ee5fb7152304b4f5f6696",
+    "url": "/ReactJs-Website/static/media/99.50c26525.png"
+  },
+  {
+    "revision": "d24567aa3963f7f0ba924b212422a01d",
+    "url": "/ReactJs-Website/static/media/adobe.d24567aa.svg"
+  },
+  {
+    "revision": "63044be63ef206c9d8256bca83b123aa",
+    "url": "/ReactJs-Website/static/media/amazon.63044be6.svg"
+  },
+  {
+    "revision": "5706cdca0fa8f0e59cb60116dbb43a99",
+    "url": "/ReactJs-Website/static/media/e-commerce.5706cdca.svg"
+  },
+  {
+    "revision": "cca4de65bbacac90a8bde50d10775bac",
+    "url": "/ReactJs-Website/static/media/ebay.cca4de65.svg"
+  },
+  {
+    "revision": "f0730d3ce79bfbdd475c28a5110542c2",
+    "url": "/ReactJs-Website/static/media/graphic.f0730d3c.svg"
+  },
+  {
+    "revision": "bc9c1c7adf2c6e0a2d797236da8fd5f1",
+    "url": "/ReactJs-Website/static/media/ideas.bc9c1c7a.svg"
+  },
+  {
+    "revision": "9db8745a791333a2b739dd40ea9d991d",
+    "url": "/ReactJs-Website/static/media/logo.9db8745a.png"
+  },
+  {
+    "revision": "3e114da3f8dd138e42ba6fdf2c4d78de",
+    "url": "/ReactJs-Website/static/media/main.3e114da3.svg"
+  },
+  {
+    "revision": "1d8ab59b562f139e7e4727c9a6b342c9",
+    "url": "/ReactJs-Website/static/media/marketing.1d8ab59b.svg"
+  },
+  {
+    "revision": "364ae2806c8ff58ca40a2c6aee16ea72",
+    "url": "/ReactJs-Website/static/media/product.364ae280.svg"
+  },
+  {
+    "revision": "888fa8d5623984b5232a9ae04707b35b",
+    "url": "/ReactJs-Website/static/media/samsung.888fa8d5.svg"
+  },
+  {
+    "revision": "3a4041db2b8127fc20b3306c4ed46305",
+    "url": "/ReactJs-Website/static/media/security.3a4041db.svg"
+  }
+]);
